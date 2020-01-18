fa95950d4760cbd73f7c636995d0f55ec8e64d56
Mirko Gonzalez <mirko.gonzalez@xintec.cl>
2020-01-18 17:28:11 -0300

    falta implementar deploy con heroku

--- src/app/pages/web/home/home.component.ts
+++ src/app/pages/web/home/home.component.ts
@@ -1,3 +1,4 @@
+import { FarmaciaServiceClient } from './../../../shared/services/farmacias.service';
 import { Component, OnInit } from "@angular/core";
 
 @Component({
@@ -7,9 +8,38 @@ import { Component, OnInit } from "@angular/core";
 })
 export class HomeComponent implements OnInit {
 
-    constructor() { }
+    comunas: any;
+    comunaSeleccionada: any = 0;
+    farmaciaBuscar: any;
+    constructor(private farmaciaService: FarmaciaServiceClient) { }
 
-    ngOnInit() {
+    async ngOnInit() {
+        this.comunas = await this.obtenerComunas();
 
+    }
+
+    async obtenerComunas() {
+        return this.farmaciaService.obtenerComunasSantiago();
+    }
+
+    async eliminarPrimerDatoArray(array) {
+
+
+        return array;
+    }
+
+    async buscarFarmacia(comuna, nombre) {
+        this.farmaciaService.obtenerFarmaciasPorComunaYNombrew(comuna, nombre).then((farmacias) => {
+            console.log(farmacias);
+        }).catch((err) => {
+            console.log('Error al realizar la consulta', err);
+        })
+    }
+
+    onKeydown(event) {
+        if (event.key === "Enter") {
+            console.log(this.farmaciaBuscar, this.comunaSeleccionada);
+            this.buscarFarmacia(this.comunaSeleccionada, this.farmaciaBuscar);
+        }
     }
 }
--- src/app/shared/services/farmacias.service.ts
+++ src/app/shared/services/farmacias.service.ts
@@ -8,6 +8,22 @@ export class FarmaciaServiceClient {
     constructor(private http: HttpClient){ }
 
     obtenerComunasSantiago(){
-        return this.http.get('').toPromise()
+        return this.http.get('http://localhost:8080/v1/api/farmacias/obtenerComunas').toPromise()
+    }
+
+    /**
+     * 
+     * @param comuna comuna que se desea filtrar
+     * @param nombre nombre de la farmacia abuscar
+     */
+    obtenerFarmaciasPorComunaYNombrew(comuna, nombre){
+
+        let body = {
+            comuna: comuna,
+            nombre_de_local: nombre.toUpperCase()
+        }
+
+        debugger;
+        return this.http.post(`http://localhost:8080/v1/api/farmacias/obtenerFarmacias`, body).toPromise();
     }
 }
